83faf14d14f9d1d402ba1a5c5e489763bac3bf8b
EJ <evjarlos@gmail.com>
2024-10-24 23:23:29 -0600

    First commit

---
+++ mainpractice/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Making a comment
+/*
+    Multi
+    Line
+    Comment
+*/
+//Write and it will go to next line of the console
+Console.WriteLine("Hello, World!");
+
+//Generic variable that can be most types
+var num1 = 1;
+var num2 = 4;
+
+//Write but anything written after will be on one line/same line
+Console.Write("Adding 2 variables: ");
+Console.Write(num1+num2);
+
+//types of variables
+/*
+    initializing a variable usually goes like:
+    type variableName = value;
+*/
+int wholeNum = 8; //int stores whole numbers (no decimals)
+double decimalNum = 6.4; //double stores floating point numbers (decimals)
+char alphabetLetter = 'V'; //char stores single characters, needs to be surrounded by single quotes ('')
+string longWord = "Multiple words can be stored"; //string stores text, surrounded by double quotes("")
+bool isItTrue = false; //bool stores either true OR false
